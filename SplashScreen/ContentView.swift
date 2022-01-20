5277dce81a0aeec1b1ba1d97a636fa23f0a6e01b
Code Palace <codepalace@fastmail.se>
2022-01-20 11:55:06 +0100

    Initial Commit

---
+++ SplashScreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SplashScreen
+//
+//  Created by Federico on 20/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplashScreen/SplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenApp.swift
+//  SplashScreen
+//
+//  Created by Federico on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
